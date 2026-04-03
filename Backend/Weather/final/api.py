47baabde33d237c3addc84251d6abed5063c8e03
Neelam Nagar <neelamnagar526@gmail.com>
2026-04-03 09:02:51 +0530

    weather api ready

--- Backend/Weather/final/api.py
+++ Backend/Weather/final/api.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fetch_weather import get_weather_for_chatbot
+import os
 
 app = FastAPI()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(BASE_DIR, "final_clean.json")
 
 @app.get("/")
 def home():
@@ -10,15 +14,11 @@ def home():
 @app.get("/weather")
 def weather(district: str, tehsil: str, village: str):
     try:
-        result = get_weather_for_chatbot(
-            "final_clean.json",
+        return get_weather_for_chatbot(
+            json_path,
             district,
             tehsil,
             village
         )
-        return result
     except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }
+        return {"error": str(e)}
